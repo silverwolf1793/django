95c39160e4f05b2020b09d8af8bc9ae9837929e6
Eric <eric.lopez@teamknowlogy.com>
2020-06-18 19:39:48 -0500

    code cleanup

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(Custumer)
-admin.site.register(Product)
-admin.site.register(Tag)
-admin.site.register(Order)
